a04cec4e0e6f2ecb1e012eb0a4afb3660c7c4a9d
mateuszreczulski <reiczel@proton.me>
2024-04-15 22:44:07 +0200

    Register, RegisterEquipment(just created not done)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import Image from "next/image";
-import {Button} from '@nextui-org/button';
-import LogIn from "@/components/LogIn";
+import Image from 'next/image';
+import { Button } from '@nextui-org/button';
+import LogIn from '@/components/LogIn';
+import Register from '@/components/Register';
+import EquipmentRegistration from '@/components/EquipmentRegistration';
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="w-1/3">
-          <LogIn  />
-        </div>
-      </main>
+    <main className='flex flex-col items-center'>
+      <div className=''>
+        <LogIn />
+      </div>
+      <div>
+        <Register />
+      </div>
+      <div>
+        <EquipmentRegistration />
+      </div>
+    </main>
   );
 }
--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -1,49 +1,50 @@
-import React from "react";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Input, Button} from "@nextui-org/react";
+import React from 'react';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+  Input,
+  Button,
+} from '@nextui-org/react';
 
-export default function LogIn(){
-
-    return(
-        <Card className="">
-            <CardHeader className="flex gap-4 w-full">
-                <Image
-                    alt="nextui logo"
-                    height={40}
-                    radius="sm"
-                    src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-                    width={40}
-                />
-                <div className="flex flex-col">
-                    <p className="text-md">Logowanie</p>
-                    <p className="text-small text-default-500">LIFTER</p>
-                </div>
-            </CardHeader>
-            <Divider/>
-            <CardBody className="items-center w-full">
-                <Input
-                    isRequired
-                    type="email"
-                    label="Email"
-                    className="mt-4"
-                />
-                <Input
-                    isRequired
-                    type="password"
-                    label="Hasło"
-                    className="mt-2"
-                />
-                <Button color="primary" className="w-1/2 p-2 mt-3">Zaloguj się</Button>
-            </CardBody>
-            <Divider/>
-            <CardFooter className="w-full">
-                <Link
-                    isExternal
-                    showAnchorIcon
-                    href="https://github.com/nextui-org/nextui"
-                >
-                    Zarejestruj się.
-                </Link>
-            </CardFooter>
-        </Card>
-    );
+export default function LogIn() {
+  return (
+    <Card className='max-w-96 items-center'>
+      <CardHeader className='flex w-full flex-col items-center gap-4 md:flex-row'>
+        <Image
+          alt='nextui logo'
+          height='max-h-32'
+          radius='sm'
+          src='/Lifter-logo-only.png'
+          width='w-auto'
+        />
+        <div className='flex flex-col pr-6'>
+          <p className='text-xl'>Logowanie</p>
+          <p className='text-lg text-default-500'>LIFTER</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody className='w-full max-w-80 items-center'>
+        <Input isRequired type='email' label='Email' className='mt-4' />
+        <Input isRequired type='password' label='Hasło' className='mb-5 mt-4' />
+        <Button color='primary' className='w-full md:w-1/2'>
+          Zaloguj się
+        </Button>
+      </CardBody>
+      <Divider />
+      <CardFooter className='w-full'>
+        <Link
+          isExternal
+          showAnchorIcon
+          href='https://github.com/nextui-org/nextui'
+        >
+          Zarejestruj się.
+        </Link>
+      </CardFooter>
+    </Card>
+  );
 }
